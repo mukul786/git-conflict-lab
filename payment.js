7703ac7982e1e0c85621a4337e9b9cdd978f0b03
Mukul Upadhyay <mukul.upadhyay486@gmail.com>
2026-05-12 11:30:32 +0000

    Master card has been added

--- payment.js
+++ payment.js
@@ -3,5 +3,6 @@ function payment () {
 	console.log("Payment Started");
 	console.log("master Payment and card validation done")
 	console.logs("UPI payment Moudule")
+	console.log("Mastercard Module has been added")
 }
 
